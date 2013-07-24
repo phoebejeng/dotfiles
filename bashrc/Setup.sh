9820456ec63002c6839f85ca4824eb2391f50348
phoebejeng <phoebejeng@kkbox.com>
2013-07-24 12:17:52 +0800

    Modify bashrc setup

--- bashrc/Setup.sh
+++ bashrc/Setup.sh
@@ -1,5 +1,11 @@
 echo "====== bash ======"
 currentPath=`pwd`
+
+echo "Check config file..."
+ls ~/_backup_config || mkdir ~/_backup_config
+find ~/.bashrc && mv ~/.bashrc ~/_backup_config/_bashrc
+
 echo "Set symbolic link: ${currentPath}/_bashrc to ~/.bashrc"
 ln -s ${currentPath}/_bashrc ~/.bashrc
 echo "==== Setup end ===="
+
