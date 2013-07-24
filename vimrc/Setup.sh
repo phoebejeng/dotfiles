37f64e12ce244b747c8d1613db8017c9b61ef6b9
phoebejeng <phoebejeng@kkbox.com>
2013-07-24 12:20:59 +0800

    Modify vim setup

--- vimrc/Setup.sh
+++ vimrc/Setup.sh
@@ -1,5 +1,12 @@
 echo "====== vim ======"
 currentPath=`pwd`
+
+echo "Check config file..."
+ls ~/_backup_config || mkdir ~/_backup_config
+find ~/.vimrc && mv ~/.vimrc ~/_backup_config/_vimrc
+
+find ~/.vim && cp -r ~/.vim ~/_backup_config/_vim && rm -rf ~/.vim
+
 echo "Set symbolic link: ${currentPath}/_vimrc"
 ln -s ${currentPath}/_vimrc ~/.vimrc
 
