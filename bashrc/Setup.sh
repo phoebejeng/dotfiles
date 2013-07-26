c748cbe1039e32323e63ee79e54c7563022cd31e
phoebejeng <phoebejeng@kkbox.com>
2013-07-26 17:45:33 +0800

    Add setup file

--- bashrc/Setup.sh
+++ bashrc/Setup.sh
@@ -4,8 +4,12 @@ currentPath=`pwd`
 echo "Check config file..."
 ls ~/_backup_config || mkdir ~/_backup_config
 find ~/.bashrc && mv ~/.bashrc ~/_backup_config/_bashrc
+find ~/.bash_profile && mv ~/.bash_profile ~/_backup_config/_bash_profile
 
 echo "Set symbolic link: ${currentPath}/_bashrc to ~/.bashrc"
 ln -s ${currentPath}/_bashrc ~/.bashrc
+
+echo "Set symbolic link: ${currentPath}/_bash_profile to ~/.bash_profile"
+ln -s ${currentPath}/_bash_profile ~/.bash_profile
 echo "==== Setup end ===="
 
