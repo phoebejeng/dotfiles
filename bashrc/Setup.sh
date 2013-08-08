6da2ac08fe1db3a3296d4e199e86de408c6737f7
phoebejeng <phoebejeng@kkbox.com>
2013-08-08 11:50:41 +0800

    Don't need to check files exist

--- bashrc/Setup.sh
+++ bashrc/Setup.sh
@@ -1,15 +1,11 @@
-echo "====== bash ======"
+echo "====== <bash> ======"
 currentPath=`pwd`
 
-echo "Check config file..."
-ls ~/_backup_config || mkdir ~/_backup_config
-find ~/.bashrc && mv ~/.bashrc ~/_backup_config/_bashrc
-find ~/.bash_profile && mv ~/.bash_profile ~/_backup_config/_bash_profile
+echo "Remove config files..."
+rm ~/.bashrc
+rm ~/.bash_profile
 
-echo "Set symbolic link: ${currentPath}/_bashrc to ~/.bashrc"
+echo "Set symbolic link..."
 ln -s ${currentPath}/_bashrc ~/.bashrc
-
-echo "Set symbolic link: ${currentPath}/_bash_profile to ~/.bash_profile"
 ln -s ${currentPath}/_bash_profile ~/.bash_profile
-echo "==== Setup end ===="
-
+echo "==== Setup <bash> end ===="
